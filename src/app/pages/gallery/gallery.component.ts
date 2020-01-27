039c1e1f565b0343896f9b33b49501d78dcae8cb
hongzhi pan <victoryhongzhi@gmail.com>
2020-01-27 00:09:14 -0500

    added fun facts about llamas

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gallery.component.scss']
 })
 export class GalleryComponent implements OnInit {
-  items = [1,1,1,1,1,1];
-  constructor() { }
+  items = [
+    'I am social',
+    'I am smart',
+    'I am camel hippie cousin',
+    'I am diabetic',
+    'I humming',
+    'I am adoptive',
+    'I am cute',
+    'I am cool',
+    'I can travel for long distance',
+    'I am Llama'
+  ];
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
